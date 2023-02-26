93baa98ab5b3c7ed2645361d2e088fa6f2c67f5b
Enrique <rchavezs@uni.pe>
2023-02-26 10:05:35 -0500

    feat: Configuración del proyecto

---
+++ tests_monet/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from tests_monet.users.models import User
+from tests_monet.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ tests_monet/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from tests_monet.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
